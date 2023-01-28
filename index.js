43250ecd199ffc1ba4d418718bb3904147fafe95
meir <meirkhanabdullayev@yahoo.com>
2023-01-28 22:08:19 +0600

    wow

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const http = require('http');
-const express = require('express');
-const morgan = require('morgan');
-const {Pool} = require('pg');
-require('dotenv').config;
+// const http = require('http');
+// const express = require('express');
+// const morgan = require('morgan');
+// const {Pool} = require('pg');
+// require('dotenv').config;
 
+// const app = express()
+
+// const port = 3000;
+
+// app.use(morgan('common'))
+
+// app.listen(port, (
+//     console.log(`Server started on port ${port}`)
+// ))
+
+const express = require('express')
 const app = express()
-const port = 3000;
-app.use(morgan('common'))
-app.listen(port, (
-    console.log(`Server started on port ${port}`)
-))
+const port = 3000
+
+app.get('/', function(req, res) {
+    res.send('Hello, Abdulla!');
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
